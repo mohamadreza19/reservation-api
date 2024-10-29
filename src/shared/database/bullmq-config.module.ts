92ce88e947d368eb39ab0135257741a4ed940bf7
mohamadreza19 <mrzar1380@gmail.com>
2024-10-29 18:17:54 +0330

    REDIS_URL

--- src/shared/database/bullmq-config.module.ts
+++ src/shared/database/bullmq-config.module.ts
@@ -2,7 +2,7 @@
 import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import Redis from 'ioredis';
+import { Redis } from 'ioredis';
 
 @Module({
   imports: [
@@ -10,17 +10,16 @@ import Redis from 'ioredis';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => {
-        const redisConnection = new Redis({
-          // Use Render Redis service name as host, red-xxxxxxxxxxxxxxxxxxxx
-          host: configService.get('REDIS_SERVICE_NAME'),
-          // Default Redis port
-          port: configService.get('REDIS_PORT') || 6379,
-        });
-
         return {
           name: 'jobs',
           maxConcurrent: 1,
-          connection: redisConnection,
+          connection: {
+            // host: configService.get('REDIS_HOST'),
+            // port: configService.get('REDIS_PORT'),
+            url: configService.get('REDIS_URL'),
+            // tls: true,
+            db: 0,
+          },
 
           //   redis: {
           //     host: configService.get('REDIS_HOST'),
