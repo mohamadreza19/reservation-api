e68b5f0706e20efa744642de216edba10700ff00
mohamadreza19 <mrzar1380@gmail.com>
2024-10-29 21:17:05 +0330

    bull-qm config 9

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './features/app/app.module';
 import { SwaggerConfig } from './shared/config/SwaggerConfig';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import Redis from 'ioredis';
 
 async function bootstrap() {
   const URL_Prefix = '/api';
@@ -18,5 +19,25 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3000);
+
+  const { REDIS_SERVICE_NAME, REDIS_PORT } = process.env;
+
+  // This will only work on Render hosted services
+  const renderRedis = new Redis({
+    // Use Render Redis service name as host, red-xxxxxxxxxxxxxxxxxxxx
+    host: REDIS_SERVICE_NAME,
+    // Default Redis port
+    port: Number(REDIS_PORT) || 6379,
+  });
+
+  console.log('Connected to Render Redis! 🚀');
+
+  renderRedis.set('animal', 'mouse');
+
+  renderRedis.get('animal').then((result) => {
+    console.log(`Result for key animal: ${result}`); // Prints "mouse"
+  });
+
+  renderRedis.del('animal');
 }
 bootstrap();
